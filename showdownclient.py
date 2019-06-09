d6c124d0d74487a76227e8b85183431798e0c8f8
Alex Ayala <AlexAyala9975@hotmail.com>
2019-06-09 16:25:28 -0700

    Adjust client to not need a queue registered

--- showdownclient.py
+++ showdownclient.py
@@ -44,10 +44,7 @@ class ShowdownClient:
     
     
     async def challenge_user(self, client=None, name=None,
-            mode=RANDOM_BATTLE, team='', register=False):
-        
-        if register:
-            self.register_queue()
+            mode=RANDOM_BATTLE, team=''):
         
         if (client is not None and name is not None) \
                 or (client is None and name is None):
@@ -60,24 +57,10 @@ class ShowdownClient:
         await self.ps_ws_client.challenge_user(name, mode, team)
     
 
-    async def accept_challenge(self, mode=RANDOM_BATTLE, team='', register=False):
-        if register:
-            self.register_queue()
+    async def accept_challenge(self, mode=RANDOM_BATTLE, team=''):
 
         self.log("Attempting to accept challenge")
         await self.ps_ws_client.accept_challenge(mode, team)
-    
-
-    def register_queue(self):
-        if self.message_queue is None:
-            self.ps_ws_client.message_queue = SimpleQueue()
-    
-
-    def deregister_queue(self):
-        message_queue = self.ps_ws_client.message_queue
-        self.ps_ws_client.message_queue = None
-
-        return message_queue
 
 
     def log(self, msg, level=logging.INFO):
@@ -91,13 +74,13 @@ class ShowdownClient:
         self.username = ''.join(random.choices(characters, k=length))
     
     
-    def get_battle_id_from_queue(self, deregister=True):
+    def get_battle_id_from_queue(self, queue):
         battle_id = None
         rqid = None
 
-        while not self.message_queue.empty() \
+        while not queue.empty() \
                 and (battle_id is None):
-            msg = self.message_queue.get()
+            msg = queue.get()
             for line in msg.splitlines():
                 if rqid is None:
                     match = re.match(r'\"rqid\":[0-9]', line)
@@ -109,9 +92,6 @@ class ShowdownClient:
                 if battle_id is None and ">battle-gen7" in line:
                     battle_id = line[1:]
         
-        if deregister:
-            self.deregister_queue()
-        
         self.battle_id = battle_id.strip()
         
         return battle_id
@@ -120,27 +100,30 @@ class ShowdownClient:
     async def get_next_message(self, timeout=0.1):
         try:
             msg = await asyncio.wait_for(
-                self.ps_ws_client.receive_message(), timeout=timeout)
+                self.ps_ws_client.websocket.recv(), timeout=timeout)
+            msg = msg.replace('\u2606', '')
+            self.log("Received from websocket: {}".format(msg), logging.DEBUG)
             return msg
         except asyncio.TimeoutError:
             return None
-    
-
-    async def get_turn_actions_from_queue(self, deregister=True):
-        actions = await self.get_turn_actions(queue=self.msg_queue)
-
-        if deregister:
-            self.deregister_queue()
-        
-        return actions
 
 
-    async def get_turn_actions(self, queue=None, register=False):
+    async def get_initial_actions(self):
+        queue = SimpleQueue()
+
+        while True:
+            msg = await self.get_next_message(timeout=3)
+            if msg:
+                queue.put(msg)
+            else:
+                break
+        self.battle_id = self.get_battle_id_from_queue(queue)
+        initial_actions = await self.get_turn_actions(queue=queue)
+
+        return initial_actions
+
+    async def get_turn_actions(self, queue=None):
         actions = []
-        new_turn = False
-
-        if register:
-            self.register_queue()
 
         while True:
             if queue:
